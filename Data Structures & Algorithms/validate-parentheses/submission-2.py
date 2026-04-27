783e39d017ada3112664b15ac277d11b7d440ee8
Sai Saran Reddy <rs.saran.reddy@gmail.com>
2026-04-28 04:33:44 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,63 @@
+# You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+# The input string s is valid if and only if:
+
+# Every open bracket is closed by the same type of close bracket.
+# Open brackets are closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+# Return true if s is a valid string, and false otherwise.
+
+# Example 1:
+
+# Input: s = "[]"
+
+# Output: true
+# Example 2:
+
+# Input: s = "([{}])"
+
+# Output: true
+# Example 3:
+
+# Input: s = "[(])"
+
+# Output: false
+# Explanation: The brackets are not closed in the correct order.
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # open_set = {"(","[","{"}
+        # close_set = {")","]","}"}
+
+        pairs = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        stack = []
+
+        for c in s:
+            if c in pairs.keys():
+                if len(stack) == 0:
+                    return False
+                li = stack.pop()
+                if pairs[c] != li:
+                    return False
+            else:
+                stack.append(c)
+            
+            # print(c, stack)
+            
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+        
+
+        
